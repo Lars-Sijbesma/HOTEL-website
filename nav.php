da4ca66e7edf6962bebc70ad6ef9159f69b138e0
171650 <171650@student.talland.nl>
2026-02-02 14:19:50 +0100

    nav

---
+++ nav.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-sm bg-light">
+
+  <div class="container-fluid">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="restaurant.php">Restaurant</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contact.php">Contact</a>
+      </li>
+    </ul>
+  </div>
+
+</nav>
