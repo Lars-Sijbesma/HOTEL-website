27777eecbd32ac388047c5254c411beabd5bf764
Lars Sijbesma <183395@student.talland.nl>
2026-02-03 14:02:26 +0100

    [feature]: [carousel]
    carousel of images added

--- homepage/index.php
+++ homepage/index.php
@@ -6,17 +6,33 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/index.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="./css/index.css" rel="stylesheet">
     <title>Homepagina</title>
 </head>
-
 <body>
+<main>
 
-<!--#h1 centered-->
+
 <h1> Welkom bij hotel De Zonne Vallei</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, minima obcaecati odit possimus quibusdam reiciendis tempora. Culpa delectus dignissimos doloremque dolorum ducimus, esse est excepturi officiis omnis repellat sequi, veniam!</p></>
 
-<p class="text-center-middle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, minima obcaecati odit possimus quibusdam reiciendis tempora. Culpa delectus dignissimos doloremque dolorum ducimus, esse est excepturi officiis omnis repellat sequi, veniam!</p></>
+<!--Carousel-->
+<section id="carousel" class="carousel slide" data-bs-ride="carousel">
+        <article class="carousel-inner">
+            <p class="carousel-item active">
+                <img src="./images/hotel-presidente-4s.jpg" class="d-block w-100" alt="image failed loading">
+            </p>
+            <p class="carousel-item">
+                <img src="./images/640278495.jpg" class="d-block w-100" alt="image failed loading">
+            </p>
+            <p class="carousel-item">
+                <img src="./images/hotel-wailea-hero-scaled-1-1-1024x567.webp" class="d-block w-100" alt="image failed loading">
+            </p>
+    </article>
+</section>
 
 
+</main
 </body>
 </html>
