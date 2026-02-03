850f7da20e4e552619eaef775c723285dc32e206
Lars Sijbesma <183395@student.talland.nl>
2026-02-03 09:34:58 +0100

    index html, index css, bootsrap added and created

---
+++ homepage/index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/index.css" rel="stylesheet">
+    <title>Homepagina</title>
+</head>
+
+<body>
+
+<!--#h1 centered-->
+<h1> Welkom bij hotel De Zonne Vallei</h1>
+
+<p class="text-center-middle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, minima obcaecati odit possimus quibusdam reiciendis tempora. Culpa delectus dignissimos doloremque dolorum ducimus, esse est excepturi officiis omnis repellat sequi, veniam!</p></>
+
+
+</body>
+</html>
