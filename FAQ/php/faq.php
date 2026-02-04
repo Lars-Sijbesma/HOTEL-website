7a1ed3f7efa3ee02afe1d935f77d2ecc643cf6b1
GitTycho <121061296+GitTycho@users.noreply.github.com>
2026-02-04 10:10:25 +0100

    FAQ bases

---
+++ FAQ/php/faq.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/global.css">
+    <link rel="stylesheet" href="../css/faq.css">
+</head>
+
+<body>
+    <header class="navbar">
+        <img src="../images/logo variant 2.png" alt="logo" class="logo">
+        <nav class="nav-links"><a href="homepage.php">Home</a><a href="restaurant.php">Restaurant</a><a href="kamers.php">Kamers</a><a href="over-ons.php">Over ons </a><a href="contact.php">Contact
+            </a></nav>
+    </header>
+    <main>
+        <h1>General</h1>
+        <p>Booking Policy</p>
+        <p>Payment</p>
+        <p>Smoking</p>
+
+        <h1>Hotel</h1>
+        <p>Check-in/Check-out</p>
+        <p>Parking</p>
+        <p>Amenities</p>
+        <p>Special Requests</p>
+
+        <h1>Restaurant</h1>
+        <p>Reservations</p>
+        <P>Hours</P>
+        <p>Menu</p>
+        <p>Events</p>
+        <p>Takeaway</p>
+    </main>
+</body>
+
+</html>
