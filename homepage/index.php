00dae78677dbbfe0ed57df6cfe1bbc62576ee041
Lars Sijbesma <183395@student.talland.nl>
2026-02-04 10:20:05 +0100

    [Add]: [navbar]

--- homepage/index.php
+++ homepage/index.php
@@ -8,20 +8,37 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <link href="./css/index.css" rel="stylesheet">
-    <title>Homepagina</title>
+    <title>Home pagina</title>
 </head>
 <body>
 <main>
 
+<nav>
+    <a>
+        Home pagina
+    </a>
+    <a>
+        Restaurant
+    </a>
+    <a>
+        Kamers
+    </a>
+    <a>
+        Over ons
+    </a>
+    <a>
+        Contact pagina
+    </a>
+</nav>
 
 <h1> Welkom bij hotel De Zonne Vallei</h1>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, minima obcaecati odit possimus quibusdam reiciendis tempora. Culpa delectus dignissimos doloremque dolorum ducimus, esse est excepturi officiis omnis repellat sequi, veniam!</p></>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, minima obcaecati odit possimus quibusdam reiciendis tempora. Culpa delectus dignissimos doloremque dolorum ducimus, esse est excepturi officiis omnis repellat sequi, veniam!</p>
 
 <!--Carousel-->
 <section id="carousel" class="carousel slide" data-bs-ride="carousel">
         <article class="carousel-inner">
             <p class="carousel-item active">
-                <img src="./images/hotel-presidente-4s.jpg" class="d-block w-100" alt="image failed loading">
+                <img src="./images/itzana-resort-and-residences.webp" class="d-block w-100" alt="image failed loading">
             </p>
             <p class="carousel-item">
                 <img src="./images/640278495.jpg" class="d-block w-100" alt="image failed loading">
@@ -31,8 +48,6 @@
             </p>
     </article>
 </section>
-
-
-</main
+</main>
 </body>
 </html>
