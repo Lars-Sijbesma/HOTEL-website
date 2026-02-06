bf3049357ddcbe57f50d092781794919033752c9
Tycho <121061296+GitTycho@users.noreply.github.com>
2026-02-06 13:21:01 +0100

    Add files via upload

---
+++ contact-page/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <header class="navbar">
+        <img src="../images/logo variant 2.png" alt="logo" class="logo">
+        <nav class="nav-links"><a href="homepage.php">Home</a><a href="restaurant.php">Restaurant</a><a
+                href="kamers.php">Kamers</a><a href="over-ons.php">Over ons </a><a href="contact.php">Contact</a><a
+                href="faq.php">FAQ</a>
+            </a></nav>
+        </header>
+        <select id="languageSelect">
+    <option value="nl">Nederlands</option>
+    <option value="en">English</option>
+</select>
+</body>
+
+</html>
